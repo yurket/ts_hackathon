b4bf49fed40fcb940f1942868ceb050686f888d6
yurket <yurket2006@yandex.ru>
2021-06-15 12:15:32 +0300

    Going through the tutorial - add test

--- go_server/main_test.go
+++ go_server/main_test.go
@@ -3,10 +3,10 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"io/ioutil"
 )
 
 func TestHandler(t *testing.T) {
@@ -91,5 +91,35 @@ func TestRouter(t *testing.T) {
 	if respString != expected {
 		t.Errorf("Response should be %s, got %s", expected, respString)
 	}
+}
 
+func TestRouterForNonExistentRoute(t *testing.T) {
+	r := newRouter()
+	mockServer := httptest.NewServer(r)
+	// Most of the code is similar. The only difference is that now we make a
+	//request to a route we know we didn't define, like the `POST /hello` route.
+	resp, err := http.Post(mockServer.URL+"/hello", "", nil)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// We want our status to be 405 (method not allowed)
+	if resp.StatusCode != http.StatusMethodNotAllowed {
+		t.Errorf("Status should be 405, got %d", resp.StatusCode)
+	}
+
+	// The code to test the body is also mostly the same, except this time, we
+	// expect an empty body
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	respString := string(b)
+	expected := ""
+
+	if respString != expected {
+		t.Errorf("Response should be %s, got %s", expected, respString)
+	}
 }
