1b6f7fa2b2292a946ecbeb1e2538265b1f816a3f
yurket <yurket2006@yandex.ru>
2021-06-18 21:07:36 +0300

    Add #! string and +x

--- send.py
+++ send.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import requests
 import random
 import argparse
@@ -9,7 +12,7 @@ def send_data():
     url = 'http://localhost:8080/'
     myobj = f'{{"Time":"{randint()}","Temperature":"{randint()}","Pressure":"{randint()}","Humidity":"{randint()}"}}'
     headers = {'Content-Type': 'application/json'}
-    
+
     x = requests.post(url, data=myobj, headers=headers)
     print(x.text)
 
